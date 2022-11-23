dcdc057aa4ff688980d9a92f04c9c63ba58acb83
Eric Allard <21221061+everlongtrail@users.noreply.github.com>
2022-11-22 19:59:19 -0500

    added dotfiles config alias and code to pull down dotfiles

--- install.sh
+++ install.sh
@@ -26,6 +26,20 @@ git config --global user.name "Eric Allard"
 
 # Pull down .dotfiles from GitHub (everlongtrail/.dotfiles)
 git clone --bare git@github.com:everlongtrail/.dotfiles.git $HOME/.dotfiles
+alias config='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
+function config {
+   /usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+mkdir -p .config-backup
+config checkout
+if [ $? = 0 ]; then
+  echo "Checked out config.";
+  else
+    echo "Backing up pre-existing dot files.";
+    config checkout 2>&1 | egrep "\s+\." | awk {'print $1'} | xargs -I{} mv {} .config-backup/{}
+fi;
+config checkout
+config config status.showUntrackedFiles no
 
 function install {
   which $1 &> /dev/null
